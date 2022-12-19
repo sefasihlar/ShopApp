87727d7df029745158a4eee16920e89b8217c908
ayyıldız@software <sihlarsefa7@gmail.com>
2022-12-20 02:13:11 +0300

    Created admin/product-list

--- ShopApp/Controllers/AdminController.cs
+++ ShopApp/Controllers/AdminController.cs
@@ -1,13 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopApp.Business.Concrete;
+using ShopApp.DataAccess.Abstract;
+using ShopApp.DataAccess.Concrete.EfCore;
 using ShopApp.Entites;
+using ShopApp.WebUI.Models;
 
 namespace ShopApp.WebUI.Controllers
 {
     public class AdminController : Controller
     {
+        ProductManager ip = new ProductManager(new EfCoreProductDal());
         public IActionResult Index()
         {
-            return View();
+            return View(new ProductModel()
+            {
+                Products = ip.GetALl()
+            });
         }
         [HttpGet]
         public IActionResult CreateProdcut()
@@ -16,8 +24,18 @@ namespace ShopApp.WebUI.Controllers
         }
 
         [HttpPost]
-        public IActionResult CreateProduct(Product entity)
+        public IActionResult CreateProduct(AddProductModel model)
         {
+            var values = new Product()
+            {
+                Name = model.Name,
+                Price = model.Price,
+                Gender = model.Gender,
+                ImageUrl = model.ImageUrl,
+            };
+
+            ip.Create(values);
+
             return Redirect("Index");    
         }
     }
---
+++ ShopApp/Models/AddProductModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopApp.WebUI.Models
+{
+    public class AddProductModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public string? Gender { get; set; }
+    }
+}
